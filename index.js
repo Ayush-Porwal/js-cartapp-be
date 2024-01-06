6bb288695caf74a9e180b35c89460c9460702e57
Ayush Porwal <87272409+porwal-ayush@users.noreply.github.com>
2024-01-06 21:45:07 +0530

    adding serverless logic to the app

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
+// serverless
+import ServerlessHttp from 'serverless-http';
+
 // express related
-import cors from "cors";
-import express from "express";
-import cookieParser from "cookie-parser";
-import authRouter from "./routers/auth.js";
-import productsRouter from "./routers/products.js";
+import cors from 'cors';
+// import 'dotenv/config.js';
+import express from 'express';
+import cookieParser from 'cookie-parser';
+import authRouter from './routers/auth.js';
+import productsRouter from './routers/products.js';
 
 // database related
-import connectToDb from "./db/db.js";
+import connectToDb from './db/db.js';
 
 const app = express();
 
@@ -18,18 +22,20 @@ app.use(
   })
 );
 app.use(cookieParser());
-app.use(express.json({ limit: "16kb" }));
+app.use(express.json({ limit: '16kb' }));
 app.use(express.urlencoded({ extended: true }));
 
 // routers
-app.use("/auth", authRouter);
-app.use("/products", productsRouter);
+app.use('/auth', authRouter);
+app.use('/products', productsRouter);
 
 // once db is connected, start the server
 connectToDb()
   .then(() => {
     app.listen(process.env.SERVER_PORT, () => {
-      console.log("Server initialized on port: " + process.env.SERVER_PORT);
+      console.log('Server initialized on port: ' + process.env.SERVER_PORT);
     });
   })
-  .catch((error) => console.log("Failed to start the server", error));
+  .catch((error) => console.log('Failed to start the server', error));
+
+export const handler = ServerlessHttp(app);
